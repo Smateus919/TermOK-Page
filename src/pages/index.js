36469b17c52b8c4f436659a600bd92634065440d
Sebastian Mateus <mateuss919@gmail.com>
2021-02-15 23:53:25 -0500

    Agragando programacion de slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,14 @@ console.log('Holaaaaaaaaaaa')
 window.addEventListener('resize', function(){
     width = sliderIndividual[0].clientWidth
 })
-
-
+// setInterval(Slider, 3000)
 function Slider(){
-    slider.style.transform = "translate("+(-width*contador)+"px"
+    slider.style.transform = "translate("+(-width*contador)+"px)"
+    slider.style.transition = "transform .8s"
     contador++
+    if(contador==sliderIndividual.length+1){
+        slider.style.transform = "translate(0px)"
+        slider.style.transition = "transform .3s"
+        contador=1
+    }
 }
