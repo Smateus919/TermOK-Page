b3bd019599e4d1ccd6d6956d3bfcd10345af6dde
Sebastian Mateus <mateuss919@gmail.com>
2021-03-10 00:54:59 -0500

    Se finaliza progrmacion de slider agregando boton de next y prev slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,20 @@ let slider = document.querySelector('.slider-container')
 let sliderIndividual = document.querySelectorAll('.slider-items')
 let contador = 1
 let width = sliderIndividual[0].clientWidth
-console.log('Holaaaaaaaaaaa')
 window.addEventListener('resize', function(){
     width = sliderIndividual[0].clientWidth
 })
-// setInterval(Slider, 3000)
-function Slider(){
+
+let btnNextSlider = document.querySelector('.right-arrow')
+btnNextSlider.addEventListener('click', nextSlider) 
+let btnPrevSlider = document.querySelector('.left-arrow')
+btnPrevSlider.addEventListener('click', prevSlider)
+
+// setInterval(nextSlider, 3000)
+console.log(contador)
+
+function nextSlider(){
+    console.log(contador)
     slider.style.transform = "translate("+(-width*contador)+"px)"
     slider.style.transition = "transform .8s"
     contador++
@@ -15,5 +23,21 @@ function Slider(){
         slider.style.transform = "translate(0px)"
         slider.style.transition = "transform .3s"
         contador=1
-    }
+    }    
+    console.log(contador)
+}
+
+function prevSlider(){
+    console.log(contador)
+    if(contador==1){
+        slider.style.transform = "translate("+(-width*(sliderIndividual.length-1))+"px)"
+        slider.style.transition = "transform .8s"
+        contador=sliderIndividual.length  
+        console.log(contador)
+    }else{
+        contador=contador-2
+        slider.style.transform = "translate("+(-width*contador)+"px)"
+        slider.style.transition = "transform .8s"
+        contador++
+    }    
 }
